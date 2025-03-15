7abf3c095d4d727eb18db0659820e51392bbd16d
yuni0725 <yuni0725@naver.com>
2025-03-15 20:30:39 +0900

    v0.0.2

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,13 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sentry-debug/", trigger_error),
     path("api/v1/voca/", include("vocas.urls")),
     path("api/v1/test/", include("tests.urls")),
 ]
